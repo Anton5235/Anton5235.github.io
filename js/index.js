ecc7379d065dca5d5984062afcc8932b0b33ad78
Антон <fisher5235@yandex.ru>
2023-09-07 01:33:29 +0300

    1

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const API_BASE_URL = "https://jscp-diplom.netoserver.ru/"
+
+async function getRequest(endpoint, params = {}) {
+  const url = new URL(endpoint, API_BASE_URL) // Example => https://example.com/movies
+  url.searchParams = new URLSearchParams(params) // Example => https://example.com/movies?sessionId=1
+
+  const response = await fetch(url, {
+    method: "GET",
+    // headers: {}
+  })
+  const responseJson = await response.json()
+  return responseJson
+}
+
+async function postRequest(endpoint, payload, params = {}) {
+  const url = new URL(endpoint, API_BASE_URL)
+  url.searchParams = new URLSearchParams(params)
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(payload)
+  })
+  const responseJson = await response.json()
+  return responseJson
+}
+
+getRequest("/movies", {
+  place: 43,
+})
+
+postRequest("/book", {
+  sessionId: 1,
+  places: [45, 46]
+})
