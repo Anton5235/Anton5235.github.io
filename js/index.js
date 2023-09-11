c6b39f557035e16040871f294f91f51edb8fbdd3
Антон <fisher5235@yandex.ru>
2023-09-11 18:38:27 +0300

    1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://jscp-diplom.netoserver.ru/"
+/*const API_BASE_URL = "https://jscp-diplom.netoserver.ru/"
 
 async function getRequest(endpoint, params = {}) {
   const url = new URL(endpoint, API_BASE_URL) // Example => https://example.com/movies
@@ -33,4 +33,38 @@ postRequest("/book", {
   places: [45, 46]
 })
 
-sdnojckgkhb
+sdnojckgkhb*/
+
+async function fetchData(url, requestData, callback) {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: requestData,
+    });
+
+    if (response.status === 404) {
+      throw new Error("Resource not found");
+    } else if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    let data;
+    try {
+      data = await response.json();
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+      return; // Останавливаем выполнение функции
+    }
+
+    if (typeof callback === 'function') {
+      callback(data);
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+export default fetchData;
