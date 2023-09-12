f0f4eda4a066370cdce8c0c8c63b98dbe6961c1c
Антон <fisher5235@yandex.ru>
2023-09-13 01:50:44 +0300

    1

--- js/index.js
+++ js/index.js
@@ -1,9 +1,49 @@
-const api = fetch('https://jscp-diplom.netoserver.ru/',{
-method: 'POST',
-headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-  },
-  body: requestData,
-})
+"use strict";
 
-console.log(api)
+const sendData = async (url, data) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: JSON.stringify(data),
+
+  })
+  if(!response.ok) {
+    throw new Error('fail')
+  }
+  return await response.json()
+}
+
+sendData('https://jscp-diplom.netoserver.ru/')
+
+
+
+ /* # API сервиса
+
+### Базовый URL АПИ:
+[https://jscp-diplom.netoserver.ru/](https://jscp-diplom.netoserver.ru/)
+
+### Ответы API
+Ответы API  приходят в формате JSON
+Чтобы распарсить ответ используйте функцию [JSON.parse()](https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse)
+
+### Запросы к API
+
+При отправке запросов используйте следующие параметры:
+
+* ***Адрес (URL)*** - `https://jscp-diplom.netoserver.ru/`
+* ***Метод (Method)*** - `POST`
+*  ***HTTP заголовок (HttpRequest)***
+
+|Название заголовка | Значение заголовка |
+|--|--|
+| `Content-Type` | `application/x-www-form-urlencoded`  |
+
+### Основные функции API
+
+1.  [Получение списков всех Залов Кинофильмов и Сеансов](api/getlist.md)
+2.  [Получение актуальной схемы посадочных мест на выбранный сеанс](api/getconfig.md)
+3.  [Заказ билета](api/reserv.md)
+
+*/
