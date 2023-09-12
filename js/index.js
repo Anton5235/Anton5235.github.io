da377cc048ffadb5c0800e41d7edcbb0b439787d
Антон Рыбкин <fisher5232@yandex.ru>
2023-09-12 18:56:39 +0300

    1

--- js/index.js
+++ js/index.js
@@ -1,9 +1,64 @@
-const api = fetch('https://jscp-diplom.netoserver.ru/',{
-method: 'POST',
-headers: {
-    "Content-Type": "application/x-www-form-urlencoded",
-  },
-  body: requestData,
+const API_BASE_URL = "https://jscp-diplom.netoserver.ru/"
+
+
+
+  const response = await fetch(API_BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: "event=update",
+  })
+  const responseJson = await response.json()
+  console.log(responseJson)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const API_BASE_URL = "https://jscp-diplom.netoserver.ru/"
+
+async function getRequest(endpoint, params = {}) {
+  const url = new URL(endpoint, API_BASE_URL) // Example => https://example.com/movies
+  url.searchParams = new URLSearchParams(params) // Example => https://example.com/movies?sessionId=1
+
+  const response = await fetch(url, {
+    method: "GET",
+    // headers: {}
+  })
+  const responseJson = await response.json()
+  return responseJson
+}
+
+async function postRequest(endpoint, payload, params = {}) {
+  const url = new URL(endpoint, API_BASE_URL)
+  url.searchParams = new URLSearchParams(params)
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(payload)
+  })
+  const responseJson = await response.json()
+  return responseJson
+}
+
+getRequest("/movies", {
+  place: 43,
 })
 
-console.log(api)
+postRequest("/book", {
+  sessionId: 1,
+  places: [45, 46]
+})
